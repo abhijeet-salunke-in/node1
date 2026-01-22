6402a9ef6cccb4d294040cfbe501d59e6f312cb2
Abhijeet Salunke <abhijeetsalunke16@gmail.com>
2026-01-22 08:29:15 +0530

    Update response message in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Codingwale...')
+  res.send('Hello Sir This is node1 Deployed Successfully...')
 })
 
 app.listen(port,'0.0.0.0', () => {
